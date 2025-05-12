1734eebf3038c794aa0ecf5c20b18611538b7be3
Ahmedyassernti <ahmedyassernti2212@gmail.com>
2025-05-12 16:37:01 +0300

    counter app class

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+import CounterClass from './components/counterclass/index';
+
+function App() {
+  return (
+    <div className="App">
+      <CounterClass />
+    </div>
+  );
+}
+
+
+export default App;
---
+++ src/components/counterclass/index.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import classes from './styles.module.css';
+
+class CounterClass extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0
+        };
+    }
+
+    increment = () => {
+        this.setState(prevState => ({
+            count: prevState.count + 1
+        }));
+    }
+
+    decrement = () => {
+        this.setState(prevState => ({
+            count: prevState.count - 1
+        }));
+    }
+
+    render() {
+        return (
+            <header className={classes['App-header']}>
+                <h1>Counter App</h1>
+                {
+                <h1 className={classes['greet-container']}>
+                    {this.state.count <= 5 ? <span> count is smaller than 5</span> : <span> count is bigger than 5</span>}
+                </h1>
+                }
+                <div className={classes['counter-container']}>
+                    <button onClick={this.decrement}>-</button>
+                    <span className={classes.counter}>{this.state.count}</span>
+                    <button onClick={this.increment}>+</button>
+                </div>
+            </header>
+        );
+    }
+}
+
+export default CounterClass; 
